249eb996b5fe3e3ee7059dd09b06525255845ba8
attila.eisl <attila.eisl.student@htl-hallein.at>
2024-05-14 09:47:55 +0200

    Add Attila to HelloThomas.java

--- HelloThomas.java
+++ HelloThomas.java
@@ -3,6 +3,10 @@ public class HelloThomas
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Thomas");
+		System.out.println("Hello Attila");
 		System.out.println("Hello mLoDar");
+		
+		
+		
 	}
 }
