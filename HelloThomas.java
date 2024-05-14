c3c18c6ac412021ed28cef6345791cac27e009dd
mLoDar <mlodarcoding@gmail.com>
2024-05-14 09:36:25 +0200

    Improved general code

--- HelloThomas.java
+++ HelloThomas.java
@@ -1,8 +1,8 @@
-
-
-public class HelloThomas {
-	
-	public static void main(String[] args) {
+public class HelloThomas
+{	
+	public static void main(String[] args)
+	{
 		System.out.println("Hello Thomas");
+		System.out.println("Hello mLoDar");
 	}
 }
